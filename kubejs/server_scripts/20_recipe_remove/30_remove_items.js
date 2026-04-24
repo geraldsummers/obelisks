9406a951799c174b08883f9aad06b3eb09d9d589
geraldsummers <laughlanfahey@gmail.com>
2026-04-24 18:27:57 +1000

    Update class kits, dotcoin wallet defaults, and kettle rune swap

--- kubejs/server_scripts/20_recipe_remove/30_remove_items.js
+++ kubejs/server_scripts/20_recipe_remove/30_remove_items.js
@@ -1,8 +1,16 @@
 // kubejs/server_scripts/disable_items_recipes.js
 
 const DISABLED_ITEMS = [
-    "fallout_wastelands_:steel_ingot"
-// "minecraft:diamond",
+    "fallout_wastelands_:steel_ingot",
+    "occultism:miner_debug_unspecialized",
+    "ars_nouveau:ritual_flight",
+    "bloodmagic:telepositionsigil",
+    "ars_nouveau:stable_warp_scroll",
+    "ars_nouveau:warp_scroll",
+    "occultism:miner_foliot_unspecialized",
+    "occultism:miner_djinni_ores",
+    "occultism:miner_afrit_deeps",
+    "occultism:miner_marid_master"
 ];
 
 ServerEvents.recipes(event => {
